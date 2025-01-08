553f1fb624cb47efcb642779fbe2d9567fb2d4c8
marty957 <fantuzzimartina@yahoo.it>
2025-01-08 16:10:11 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import it.epicode.ClasseUno;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+
+    public static boolean stringaPariDispari (String test){
+      int x =test.length() % 2;
+      boolean a= false;
+        if(x==0){
+            System.out.println(!a);
+            return !a;
+        }else{
+
+            System.out.println(a); return a;}
+
+    }
+
+    public static void annoBisestile(int anno){
+
+    }
+
+
+    public static void main(String[] args) {
+stringaPariDispari("ciao");
+        ClasseUno number= new ClasseUno();
+        number.stampa(2);
+
+
+    }
 }
---
+++ src/it/epicode/ClasseUno.java
@@ -0,0 +1,23 @@
+package it.epicode;
+
+public class ClasseUno {
+
+public void stampa(int a){
+    switch (a){
+        case 1:
+            System.out.println("uno");
+            break;
+        case 2:
+            System.out.println("due");
+            break;
+        case 3:
+            System.out.println("tre");
+            break;
+        default:
+            System.out.println("numero non valido");
+            break;
+    }
+
+    }
+
+}
